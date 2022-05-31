5780e56e766a898194bfa4395f84ba2e18c1ec41
Francisco Javier Estrella Liébana <estrella@ujaen.es>
2022-05-31 12:36:53 +0200

    information verification
    Signed-off-by: Francisco Javier Estrella Liébana <estrella@ujaen.es>

--- manager.js
+++ manager.js
@@ -7,6 +7,8 @@ import { pipe } from "it-pipe";
 import { toString } from "uint8arrays/to-string";
 import { fromString } from "uint8arrays/from-string";
 
+const { createVerify, createPublicKey } = await import("node:crypto");
+
 const are_services_configured = () => {
   for (const v of ["phonendo_storage", "phonendo_verifier"]) {
     if (get_node(v) == undefined) {
@@ -16,79 +18,146 @@ const are_services_configured = () => {
   return true;
 };
 
-const triggers = {
-  phonendo_storage: {
-    connect: async (node) => {
-      setInterval(async () => {
-        let message = {
-          key: uuid4(),
-          value: {
-            field_a: uuid4(),
-            field_b: "rocks",
-          },
-        };
-        console.log("Test write", message.value);
+const dial = async (node, node_type, protocol) => {
+  const { stream } = await node.dialProtocol(
+    get_node(`phonendo_${node_type}`),
+    `/${protocol}/1.0.0`
+  );
 
-        const { stream } = await node.dialProtocol(
-          get_node("phonendo_storage"),
-          "/write/1.0.0"
-        );
-        await pipe(
-          [fromString(`${message.key}##${JSON.stringify(message.value)}`)],
-          stream,
-          async function (source) {
-            for await (const _ of source) {
-              if (are_services_configured()) {
-                await triggers.phonendo_verifier.verify(node, message.value);
-              }
-            }
-          }
-        );
-      }, 2000);
-    },
-    write: async (node, key) => {
-      const { stream } = await node.dialProtocol(
-        get_node("phonendo_storage"),
-        "/read/1.0.0"
-      );
-      await pipe([fromString(key)], stream, async function (source) {
-        for await (const data of source) {
-          const v = toString(data);
-          try {
-            console.log("Test read", JSON.parse(v));
-          } catch (error) {
-            console.error(`"${v}" is not a valid JSON object`);
-          }
-        }
-      });
-    },
+  return stream;
+};
+
+const pipe_wrapper = async (input, stream, callback) => {
+  pipe([fromString(input)], stream, async function (source) {
+    for await (const data of source) {
+      await callback(data);
+    }
+  });
+};
+
+var verifierPublicKey = undefined;
+
+// Phonendo storage triggers
+const phonendo_storage = {
+  connect: async (node) => {
+    setInterval(async () => {
+      let message = {
+        key: uuid4(),
+        value: {
+          field_a: uuid4(),
+          field_b: "rocks",
+        },
+      };
+      console.debug("Simulate capture", message.value);
+      await triggers.phonendo_storage.capture(node, message);
+    }, 5000);
   },
-  phonendo_verifier: {
-    connect: async (node) => {
-      console.log("TODO history");
-    },
-  
-    verify: async (node, message) => {
-      const { stream } = await node.dialProtocol(
-        get_node("phonendo_verifier"),
-        "/verify/1.0.0"
-      );
-      await pipe(
-        [fromString(JSON.stringify(message))],
-        stream,
-        async function (source) {
-          for await (const data of source) {
-            const v = toString(data);
-            try {
-              console.log("Verify result", JSON.parse(v));
-            } catch (error) {
-              console.error(`"${v}" is not a valid JSON object`);
-            }
-          }
+
+  capture: async (node, message) => {
+    await pipe_wrapper(
+      `${message.key}##${JSON.stringify(message.value)}`,
+      await dial(node, "storage", "capture"),
+      async () => {
+        if (are_services_configured()) {
+          await triggers.phonendo_verifier.verify(
+            node,
+            message.key,
+            message.value
+          );
         }
-      );
-    },
+      }
+    );
   },
+
+  verify: async (node, key, value) => {
+    await pipe_wrapper(
+      `${key}##${JSON.stringify(value)}`,
+      await dial(node, "storage", "verify"),
+      async () => {
+        await triggers.phonendo_publisher.publish(node, key, value);
+      }
+    );
+  },
+
+  publish: async (node, key) => {
+    await pipe_wrapper(
+      key,
+      await dial(node, "storage", "publish"),
+      async (data) => {
+        let value = toString(data);
+        console.debug("Status", value);
+      }
+    );
+  },
+};
+
+// Phonendo verifier triggers
+const phonendo_verifier = {
+  connect: async (node) => {
+    await pipe_wrapper(
+      "pk",
+      await dial(node, "verifier", "pk"),
+      async (data) => {
+        let key = toString(data);
+        verifierPublicKey = createPublicKey({
+          key,
+          type: "spki",
+          format: "pem",
+        });
+        console.debug("Public key obtained");
+      }
+    );
+    console.log("TODO verify history");
+  },
+
+  verify: async (node, key, message) => {
+    await pipe_wrapper(
+      JSON.stringify(message),
+      await dial(node, "verifier", "verify"),
+      async (data) => {
+        let value = toString(data);
+        try {
+          value = JSON.parse(value);
+          console.debug("Verified message", value);
+          let source = value.source;
+          let signature = value.signature;
+
+          const verify = createVerify("SHA256");
+          verify.write(JSON.stringify(source));
+          verify.end();
+          const verification = verify.verify(
+            verifierPublicKey,
+            signature,
+            "hex"
+          );
+          console.log("Verified result", verification);
+          if (verification) {
+            await triggers.phonendo_storage.verify(node, key, value);
+          }
+        } catch (error) {
+          console.error(`"${value}" is not a valid JSON object`);
+        }
+      }
+    );
+  },
+};
+
+// Phonendo publisher triggers
+const phonendo_publisher = {
+  connect: async (_node) => {
+    console.log("TODO connect");
+  },
+
+  publish: async (node, key, value) => {
+    console.log("TODO publish on IOTA");
+    await triggers.phonendo_storage.publish(node, key, value);
+  },
+};
+
+const triggers = {
+  phonendo_storage,
+  phonendo_verifier,
+  phonendo_publisher,
 };
 
 start(triggers).then().catch(console.error);
